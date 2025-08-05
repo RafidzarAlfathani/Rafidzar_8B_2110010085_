5d61489f24e5f0b8b10fed72dd8af07935a6aa57
RafizarAlfathani <muh.hani21@gmail.com>
2025-08-05 13:11:32 +0800

    upload ulang

--- admin/page/pesanan/hapus.php
+++ admin/page/pesanan/hapus.php
@@ -1,26 +1,37 @@
 <?php
-    $id_pesanan = $_GET['id_pesanan'];
+$id_pesanan = $_GET['id_pesanan'];
 
-    // Ambil nama file bukti bayar sebelum menghapus data dari DB
-    $ambil_bukti = $con->query("SELECT bukti_bayar FROM pesanan WHERE id_pesanan='$id_pesanan'");
-    $data_bukti = $ambil_bukti->fetch_assoc();
-    $bukti_bayar = $data_bukti['bukti_bayar'];
+// Ambil nama file bukti bayar dan bukti sampai sebelum menghapus data dari DB
+$ambil_bukti = $con->query("SELECT bukti_bayar, bukti_sampai FROM pesanan WHERE id_pesanan='$id_pesanan'");
+$data_bukti = $ambil_bukti->fetch_assoc();
+$bukti_bayar = $data_bukti['bukti_bayar'];
+$bukti_sampai = $data_bukti['bukti_sampai'];
 
-    // Hapus data dari database.
-    // Relasi ON DELETE CASCADE akan otomatis menghapus detail_pesanan & tracking_pengiriman
-    $query = "DELETE FROM pesanan WHERE id_pesanan='$id_pesanan'";
-    if ($con->query($query) === TRUE) {
-        // Jika query berhasil, hapus file fotonya dari folder
-        if (!empty($bukti_bayar)) {
-             if (file_exists("images/bukti_bayar/" . $bukti_bayar)) {
-                unlink("images/bukti_bayar/" . $bukti_bayar);
-             }
+// Hapus data dari database.
+// Relasi ON DELETE CASCADE akan otomatis menghapus detail_pesanan & tracking_pengiriman
+$query = "DELETE FROM pesanan WHERE id_pesanan='$id_pesanan'";
+if ($con->query($query) === TRUE) {
+    // Hapus file bukti_bayar jika ada
+    if (!empty($bukti_bayar)) {
+        $path_bukti_bayar = "images/bukti_bayar/" . $bukti_bayar;
+        if (file_exists($path_bukti_bayar)) {
+            unlink($path_bukti_bayar);
         }
-        echo "<script>
-                Swal.fire({ icon: 'success', title: 'Berhasil!', text: 'Data pesanan berhasil dihapus.'})
-                .then((result) => { if (result.isConfirmed) { window.location.href = '?page=pesanan'; } });
-              </script>";
-    } else {
-        echo "<script> Swal.fire({ icon: 'error', title: 'Gagal!', text: 'Terjadi kesalahan saat menghapus data.' }); </script>";
     }
+
+    // Hapus file bukti_sampai jika ada
+    if (!empty($bukti_sampai)) {
+        $path_bukti_sampai = "../../images/bukti_sampai/". $bukti_sampai;
+        if (file_exists($path_bukti_sampai)) {
+            unlink($path_bukti_sampai);
+        }
+    }
+
+    echo "<script>
+            Swal.fire({ icon: 'success', title: 'Berhasil!', text: 'Data pesanan berhasil dihapus.'})
+            .then((result) => { if (result.isConfirmed) { window.location.href = '?page=pesanan'; } });
+          </script>";
+} else {
+    echo "<script> Swal.fire({ icon: 'error', title: 'Gagal!', text: 'Terjadi kesalahan saat menghapus data.' }); </script>";
+}
 ?>
